4edd99574d8852c8b8820a58eac3ffec1219e8e1
Matis121 <mateusz6246@gmail.com>
2024-10-29 23:30:17 +0100

    feat: change div to <a> tag with href

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,9 @@ import { Toaster } from "react-hot-toast";
 function TopNav() {
   return (
     <nav className="flex items-center justify-between py-3 text-xl font-semibold border-b px-4 md:px-16 lg:px-24 bg-white shadow-sm">
-      <div className="font-bold uppercase">Fitsy</div>
+      <a className="font-bold uppercase" href="/">
+        Fitsy
+      </a>
       <div>
         <SignOutButton />
       </div>
