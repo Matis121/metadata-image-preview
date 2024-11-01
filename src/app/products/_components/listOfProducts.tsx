9bb9b45799cce759d5abaac82330e9225b9a8e20
Matis121 <mateusz6246@gmail.com>
2024-11-02 00:15:30 +0100

    fix: display images only created on your account

--- src/app/products/_components/listOfProducts.tsx
+++ src/app/products/_components/listOfProducts.tsx
@@ -3,16 +3,19 @@ import { images } from "../../../drizzle/schema";
 import { SingleProduct } from "./singleProduct";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { eq } from "drizzle-orm";
 
 export async function ListOfProducts() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
+  if (!session) {
+    return console.log("session not found");
+  }
   const userImages = await db
     .select()
     .from(images)
-    .where({ userId: session?.user.id });
+    .where(eq(images.userId, session.user.id));
 
   return (
     <div className="w-full grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
