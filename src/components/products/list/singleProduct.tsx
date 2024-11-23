4fbfa86817f29df92ed5a24068e34b0673f76950
Matis121 <mateusz6246@gmail.com>
2024-11-24 00:05:29 +0100

    refactor: create seprate component

--- src/components/products/list/singleProduct.tsx
+++ src/components/products/list/singleProduct.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { LuTrash2 } from "react-icons/lu";
 import { LuPencil } from "react-icons/lu";
 import Link from "next/link";
+import SpinnerAnimation from "@/components/spinnerAnimation";
 
 export default function SingleProduct({
   singleProduct,
@@ -26,8 +27,6 @@ export default function SingleProduct({
     setIsDeletingProduct(false);
   };
 
-  console.log(collections);
-
   return (
     <div>
       <EditProductForm
@@ -46,26 +45,7 @@ export default function SingleProduct({
         />
         {isDeletingProduct ? (
           <span className="absolute flex top-2 right-2 p-2 bg-neutral-800 text-white shadow-md rounded-md hover:bg-neutral-700 transition-all">
-            <svg
-              className="h-5 w-5 animate-spin text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+            <SpinnerAnimation />
           </span>
         ) : (
           <LuTrash2
